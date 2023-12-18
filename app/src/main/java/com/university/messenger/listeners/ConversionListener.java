fb1e29f36c02ef73f1745dc2c578e779eb87ff1f
ilya <20prorok02@gmail.com>
2023-12-19 02:19:32 +0300

    Initial commit

---
+++ app/src/main/java/com/university/messenger/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.university.messenger.listeners;
+
+import com.university.messenger.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/university/messenger/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.university.messenger.listeners;
+
+import com.university.messenger.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
